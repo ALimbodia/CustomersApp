d7eb2f04e63abdc5061a5d0abb99f859d8824480
ALimbodia <aakankshalimbodia@gmail.com>
2015-09-25 14:03:31 -0400

    updated

--- src/main/webapp/scripts/controllers.js
+++ src/main/webapp/scripts/controllers.js
@@ -1,12 +1,13 @@
-app.controller('displayController', function ($scope, $http) {
+app.controller('displayController', function ($scope, $http, $location) {
 	$scope.customers = [];
 
 	$http.get('test/customers').success(function(data) {
       $scope.customers = data;
     });
 
-	$scope.doEdit = function (customerId) {
-		
+	$scope.doEdit = function (customer) {
+		$scope.currentCustomer = customer;
+		$location.path('/update');
 	}
 
 	$scope.doDelete = function (customerId) {
